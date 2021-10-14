f573bb9ec6f789b97fdd7a9e93a155479247b7ea
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-14 20:22:44 +0530

    add basic hello world program and type of string

---
+++ main.js
@@ -0,0 +1,17 @@
+// console.log("hi rucha");
+
+//string Conversion
+
+// let value = true;
+// alert(typeof value); //boolen
+
+// value = String(value); //now value is a string "true"
+// alert(typeof value);
+
+//hear is type is string
+// let str = "123";
+// console.log(typeof str); //str
+
+//hear is type of string is number
+let num = 123;
+console.log(num); //number
