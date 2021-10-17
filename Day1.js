37a682de434761a281bdc5c763e335557df3828d
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-17 20:33:58 +0530

    add day1 challeage

---
+++ Day1.js
@@ -0,0 +1,17 @@
+//basic addition program
+function add(a, b) {
+  return a + b;
+}
+console.log(5, 9);
+//multiplication program
+function multiply(a, b) {
+  return a * b;
+}
+console.log(4, 9);
+console.log(2, 20);
+
+function calcRectArea(width, height) {
+  return width * height;
+}
+console.log(calcRectArea(5, 7));
+//expected output : 35
