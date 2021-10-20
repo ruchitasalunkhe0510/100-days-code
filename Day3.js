759d022cc31c3df0df50dd456103f346298ebb0c
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-20 23:40:18 +0530

    add Day3 file

---
+++ Day3.js
@@ -0,0 +1,16 @@
+//slice()
+//Array.prototypr => cuts a piece out of an array and return it. it starts at beginning (first parameter)
+//and stops at the end (second parameter).
+
+//Array.prototype.slice([begin[, end]])
+
+// let array1 = [1, 2, 3, 4, 5];
+// let array2 = array1.slice(2, 4);
+// console.log(array1);
+// console.log(array2);
+
+//Reverse
+
+let array1 = [1, 2, 3, 4, 5];
+let array2 = array1.slice(-2);
+console.log(array2);
