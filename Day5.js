68e7f9663b774c5feb2bb60eb49bd689c4075081
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-23 16:06:42 +0530

    add file

--- Day5.js
+++ Day5.js
@@ -14,3 +14,17 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+
+//Promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+    // Change this line
+  } else {
+    reject("Data not received");
+    // Change this line
+  }
+});
