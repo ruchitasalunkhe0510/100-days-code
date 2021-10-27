9c6fa3b8d6f79621fe71b9083e78f96220979fe9
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-27 16:32:25 +0530

    add Day10 file

---
+++ Day10.js
@@ -0,0 +1,7 @@
+let user = {
+  //an object
+  name: "John", //by key "name" store value "John"
+  age: 30, //by key "age"  store value 30
+};
+console.log(user.name);
+console.log(user.age);
--- Day9.js
+++ Day9.js
@@ -1,7 +1,7 @@
 let age = 10;
 switch (age) {
   case 18:
-    console.log("Won't work");
+    console.log("Won't work"); // the result of prompt is a string, not a number
     break;
   case "18":
     console.log("This works!");
