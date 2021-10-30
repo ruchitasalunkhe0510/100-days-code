7f64addeb3c60aa7e6c66ec26a44bfb28a36bc1a
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-30 16:46:18 +0530

    add commit

--- Day13.js
+++ Day13.js
@@ -1,4 +1,18 @@
 let user = { name: "john" };
 let admin = user;
-admin.name = "pete";
+admin.name = "pete"; //changed by the 'admin'
 alert(user.name);
+
+let user = {
+  name: "John",
+  age: 30,
+};
+let clone = {}; //the new empty object
+
+//let copy all user properties into it
+for (let key in user) {
+  clone[key] = user[key];
+}
+//now clone is fully independent object with the same content
+clone.name = "pete"; //change the data
+alert(user.name); //print john in the original object
