16b5d48bfcdf71e3b546d6c820e9b13e471b16f6
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-30 16:55:25 +0530

    new commit

--- Day13.js
+++ Day13.js
@@ -1,18 +1,28 @@
-let user = { name: "john" };
-let admin = user;
-admin.name = "pete"; //changed by the 'admin'
-alert(user.name);
+// let user = { name: "john" };
+// let admin = user;
+// admin.name = "pete"; //changed by the 'admin'
+// alert(user.name);
 
-let user = {
-  name: "John",
-  age: 30,
-};
-let clone = {}; //the new empty object
+// //Cloning and merging
 
-//let copy all user properties into it
-for (let key in user) {
-  clone[key] = user[key];
-}
-//now clone is fully independent object with the same content
-clone.name = "pete"; //change the data
-alert(user.name); //print john in the original object
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+// let clone = {}; //the new empty object
+
+// //let copy all user properties into it
+// for (let key in user) {
+//   clone[key] = user[key];
+// }
+// //now clone is fully independent object with the same content
+// clone.name = "pete"; //change the data
+// alert(user.name); //print john in the original object
+
+//object.assign
+
+let user = { name: "rahul" };
+
+Object.assign(user, { name: "Pete" });
+
+alert(user.name); //now user={name:"pete"}
