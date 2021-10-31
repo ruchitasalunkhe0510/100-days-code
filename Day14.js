33972d4e82eaad4417b2694e3c29a20dad5a756e
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-31 13:07:30 +0530

    add new keyword

--- Day14.js
+++ Day14.js
@@ -9,3 +9,15 @@ let user = new User(23);
 console.log(user.name);
 console.log(user.age);
 console.log(user.isAdmin);
+
+//use new keyword
+function User(name) {
+  if (!new.target) {
+    //if you run me without new
+    return new User(name); //
+  }
+  this.name = name;
+}
+
+let john = User("john"); //redirects call to new USER
+alert(john.name);
