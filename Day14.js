e0b07cb5c7e520e334b4c0896dc48cba30f12d4c
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-10-31 11:17:15 +0530

    new commit

--- Day14.js
+++ Day14.js
@@ -1,11 +1,11 @@
 function User(name) {
-    this.name = name;
-    //this.age = age;
-    this.isAdmin = false;
+  this.name = name;
+  this.age = age;
+  this.isAdmin = false;
 }
 let user = new User("rucha");
-//let user = new User(23);
+let user = new User(23);
 
 console.log(user.name);
-//console.log(user.age);
+console.log(user.age);
 console.log(user.isAdmin);
