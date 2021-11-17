d92ad9f0f8e9e79d9501f98fb652d36800d3e46f
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-17 20:06:45 +0530

    add some changes

--- Day16.js
+++ Day16.js
@@ -36,14 +36,14 @@
 // in provied function
 // const arr = [1, 4, 5, 2, 8, 9];
 // const arraymethod = arr.filter((num) => num > 4);
-// console.log(arraymethod);
-// console.log(arr);
+// console.log(arraymethod); //[ 5, 8, 9 ]
+// console.log(arr); //[1, 4, 5, 2, 8, 9]
 
 //map => this method create new array by calling provied function in every element.(store some data and wrtten same data in method)
 // const arr = [1, 3, 5, 6, 8, 9];
 // const oneadd = arr.map((num) => num + 1);
-// console.log(oneadd);
-// console.log(arr);
+// console.log(oneadd); //[ 2, 4, 6, 7, 9, 10 ]
+// console.log(arr); //[1, 3, 5, 6, 8, 9]
 
 //reduce => the reduce() method applies a function against an accumlator and each element in the array
 //(from left to right) to reduce it to a single value
@@ -54,31 +54,31 @@
 //some() => this method check if at least one array item passed the condition. if passed , it return 'true'otherwise 'false'
 // const arr = [1, 3, 4, 5, 7, 9];
 // const largeNum = arr.some((num) => num > 4);
-// console.log(largeNum);
+// console.log(largeNum); //true
 // const smallNum = arr.some((num) => num <= 0);
-// console.log(smallNum);
+// console.log(smallNum); //false
 
 //every() => this method check if all the array's item passed the condition. If passed, if return 'true' otherwise'false'
 // const arr = [1, 2, 3, 4, 5, 6];
 // const greter = arr.every((num) => num > 4);
-// console.log(greter);
+// console.log(greter); //false
 // const less = arr.every((num) => num < 9);
-// console.log(less);
+// console.log(less); //true
 
 //sort() => this method used to arrange/sort array's item either ascending or descending order
 // const arr = [1, 3, 4, 5, 6, 7];
 // const alpha = ["a", "e", "b", "y", "z"];
 // desc = arr.sort((a, b) => (a > b ? -1 : 1));
-// console.log(desc);
+// console.log(desc); //[ 7, 6, 5, 4, 3, 1 ]
 // asc = alpha.sort((a, b) => (a > b ? 1 : -1));
-// console.log(asc);
+// console.log(asc); //[ 'a', 'b', 'e', 'y', 'z' ]
 
 //Array.from() => this change all thing that are array-like or iterable into true array espacillay, when working with dom
 // const name = "DsCode";
 // const nameArray = Array.from(name);
-// console.log(nameArray);
-// console.log(name);
+// console.log(nameArray); //[ 'D', 's', 'C', 'o', 'd', 'e' ]
+// console.log(name);//DsCode
 
 //Array.of() => this create array from every argument passed into it.
-const num = Array.of(1, 2, 4, 5, 6);
-console.log(num);
+// const num = Array.of(1, 2, 4, 5, 6);
+// console.log(num);
