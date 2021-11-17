c33a8ebf6c4c83f2870bb1c6c8cf4bfc67c786bb
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-17 17:31:15 +0530

    add inclued methoud

--- Day16.js
+++ Day16.js
@@ -1,21 +1,33 @@
 //map
-const product = [
-  { name: "redmi", prices: [5000, 7000] },
-  { name: "vivo", prices: [8000, 9000] },
-];
-const allPrices = product.map((product) => product.prices);
-const alldata = product.map((product) => product.name);
-console.log(allPrices);
-console.log(alldata);
+// const product = [
+//   { name: "redmi", prices: [5000, 7000] },
+//   { name: "vivo", prices: [8000, 9000] },
+// ];
+// const allPrices = product.map((product) => product.prices);
+// const alldata = product.map((product) => product.name);
+// console.log(allPrices);
+// console.log(alldata);
 
 // originalArray.forEach((number, i) => {
 //   newArray[i] = number * 5;
 // });
 // console.log(newArray); //[5,10,20,25]
 
-const numbers = [1, 2, 3, 4, 5];
-numbers.forEach(consoleItem);
-function consoleItem(item, index, arr) {
-  console.log(item);
-  console.log("a[" + index + "]= " + item);
-}
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(consoleItem);
+// function consoleItem(item, index, arr) {
+//   console.log(item);
+//   console.log("a[" + index + "]= " + item);
+// }
+
+//forEach => read every element
+// const arr = [5, 7, 9, 1];
+// arr.forEach((item) => {
+//   console.log(item);
+// });
+
+//include => (In array check value is present or not return true or false)
+const arr = ["rucha", "shubu", "rahul", "baba"];
+// let checkmethoud = arr.includes("rucha"); //true
+let checkmethoud = arr.includes("sumit"); //false
+console.log(checkmethoud);
