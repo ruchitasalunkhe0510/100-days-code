a623bfcfe8bcacbd6d7446452cf15a6273b62221
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-21 00:22:13 +0530

    add some code in hoisting

--- Day17.js
+++ Day17.js
@@ -17,3 +17,17 @@ function fun() {
   console.log(name); //rucha
 }
 fun();
+
+function carDetails() {
+  //function declare top of global scope
+
+  var model; //variable declare top of local scope
+
+  model = "Audi"; //variable assign value
+
+  console.log(model); //print Audi
+}
+carDetails();
+
+//Now, we already know functions and variables declarations are hoisted,
+// but the main thing you should know that is functions are hoisted first, and then variables.
