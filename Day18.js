0672b0d52f0a0f37c75ff67f52c27b5a752cadc3
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-21 10:44:25 +0530

    add lexical scope

--- Day18.js
+++ Day18.js
@@ -5,9 +5,24 @@ function foo() {
     const fruit2 = "bannana";
     let fruit3 = "cherry";
   }
-  console.log(fruit1); //apple
+  //   console.log(fruit1); //apple
 
-  console.log(fruit2); // fruit2 is not defined
-  console.log(fruit3); //  fruit3 is not defined
+  //   console.log(fruit2); // fruit2 is not defined
+  //   console.log(fruit3); //  fruit3 is not defined
 }
 foo();
+
+//lexical scope
+function foo1() {
+  var fruit1 = "apple";
+  const fruit2 = "bannana";
+  let fruit3 = "cherry";
+  function foo2() {
+    console.log(fruit1); //apple
+
+    console.log(fruit2); // bannana
+    console.log(fruit3); //  cherry
+  }
+  foo2();
+}
+foo1();
