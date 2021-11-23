9b1fd8408a3a4217e58d6655e3316a84712e00e2
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-23 19:08:23 +0530

    add class

--- Day20.js
+++ Day20.js
@@ -1,15 +1,29 @@
-//new feature in javaScript 
+//new feature in javaScript
 
-var x = 10; // x is 10
-let x = 10;  // x is 10
-const x = 10 // x is 10
+// var x = 10; // x is 10
+// let x = 10; // x is 10
+// const x = 10; // x is 10
 
 //Arrow function : Arrow functions allows a short syntax for writing function expressions.
 // You don’t need the function keyword, the return keyword, and the curly brackets.
 
 //ES5
-var x = function(x,y) {
-    return x * y;
-}
+// var x = function (x, y) {
+//   return x * y;
+// };
 //ES6
-const x = (x, y) => x * y;
+// const x = (x, y) => x * y;
+
+//classes
+// class Car {
+//   constructor(brand) {
+//     this.carname = brand;
+//   }
+// }
+
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+}
+mycar = new car("Ford");
