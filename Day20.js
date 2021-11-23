ea4a6ceb7b2c2276a2042cb75d7b28f01aa223a8
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-23 13:22:06 +0530

    add ES6 file

---
+++ Day20.js
@@ -0,0 +1,15 @@
+//new feature in javaScript 
+
+var x = 10; // x is 10
+let x = 10;  // x is 10
+const x = 10 // x is 10
+
+//Arrow function : Arrow functions allows a short syntax for writing function expressions.
+// You don’t need the function keyword, the return keyword, and the curly brackets.
+
+//ES5
+var x = function(x,y) {
+    return x * y;
+}
+//ES6
+const x = (x, y) => x * y;
