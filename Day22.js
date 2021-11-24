44fff1eb7e5b160d8ad84ec538419eb17fab6cf8
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-24 13:12:39 +0530

    add this keyword

--- Day22.js
+++ Day22.js
@@ -1,14 +1,32 @@
 //Closures : inner function  can access to outer function variable
 
-function parent() {
-  var house = "Greencolor";
-  function child() {
-    var house = "yellowcolor";
-    var car = "tesla";
-    console.log("I have:", house, car);
-  }
-  return child;
+// function parent() {
+//   var house = "Greencolor";
+//   function child() {
+//     var house = "yellowcolor";
+//     var car = "tesla";
+//     console.log("I have:", house, car);
+//   }
+//   return child;
+// }
+// var legency = parent();
+// console.log(typeof legency);
+// legency();
+//in course of excutation parent function pass scope chain to child function as legency , and child function can acess preserved legency variable.
+
+//this keyword : The javaScript this keyword refers to object it belong to.
+//  It has different value depending on where it is used.
+//1) Alone, this refers to the global object:
+console.log(this); //window
+
+// 2) In regular function, this refers to the global object
+function sum() {
+  var add = 2 + 2;
+  console.log("sum of num:" + add); //4
+  console.log(this); //window
 }
-var legency = parent();
-console.log(typeof legency);
-legency();
+sum();
+
+//3) in a methoud , this refers to global object
+
+//4) in a function , in strict mode, this is undefined
