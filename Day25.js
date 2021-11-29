6c46ea062d4e0c6c92feea970af865b224322c47
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-29 23:04:18 +0530

    add Day25 file

---
+++ Day25.js
@@ -0,0 +1,15 @@
+//upperCase string
+// let str = "rucha";
+// console.log(str.toUpperCase());
+
+//round value
+let n = 1.234689;
+console.log(n.toFixed()); //1
+console.log(n.toFixed(2)); //1.23
+
+let num = Number("123");
+console.log(num);
+
+let str = "Hello";
+str.test = 5;
+console.log(str.test); //undefined
