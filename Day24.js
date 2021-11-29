d8290823d4878620f8203b2599efbd2974018d8b
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-30 00:18:06 +0530

    add math function

--- Day24.js
+++ Day24.js
@@ -1,8 +1,13 @@
-let user = {
-  firstName: "John",
-  sayHi() {
-    alert(`Hello, ${this.firstName}!`);
-  },
-};
+// let user = {
+//   firstName: "John",
+//   sayHi() {
+//     alert(`Hello, ${this.firstName}!`);
+//   },
+// };
 
-setTimeout(user.sayHi, 1000); // Hello, undefined!
+// setTimeout(user.sayHi, 1000); // Hello, undefined!
+
+//Math function
+//Math.random() => Returns a random number from 0 to 1 (not including 1).
+console.log(Math.random()); //0.41935718029476554
+console.log(Math.random(123)); //0.5924036404794859
