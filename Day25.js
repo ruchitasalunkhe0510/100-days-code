0017568c150db37d25de5373a4f6338142fd4c33
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-29 23:32:50 +0530

    add rounding number

--- Day25.js
+++ Day25.js
@@ -18,9 +18,9 @@ console.log(str.test); //undefined
 console.log(0xff); //255
 
 //to String(base)
-let num = 255;
-console.log(num.toString(16)); //ff
-console.log(num.toString(2)); //11111111
+// let num = 255;
+// console.log(num.toString(16)); //ff
+// console.log(num.toString(2)); //11111111
 
 //base=16 => is used for hex colors, character encoding digit 0..9 or A..F
 //base=2 => bitwise operation digit can be 0 or 1.
@@ -29,3 +29,20 @@ console.log(num.toString(2)); //11111111
 console.log((123456).toString(36)); //2n9c
 console.log((12345).toString(16)); //3039
 console.log((0).toString(2)); //0
+
+//Rounding
+// Math.floor => Rounds down: 3.1 becomes 3, and -1.1 becomes -2.
+//Math.ceil => Rounds up: 3.1 becomes 4, and -1.1 becomes -1.
+//Math.round => Rounds to the nearest integer: 3.1 becomes 3, 3.6 becomes 4, the middle case: 3.5 rounds up to 4 too.
+
+// let num = 1.2345;
+// console.log(Math.round(num * 100) / 100); //1.23
+
+// let num = 123.345;
+// console.log(Math.floor(num * 100) / 100); //123.34
+
+// let num = 12.35;
+// console.log(num.toFixed(1)); //12.3
+
+let num = 12.34;
+console.log(num.toFixed(5)); //12.34000
