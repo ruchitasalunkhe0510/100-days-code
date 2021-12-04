89cf99b2fcbab2e18ebaf161beed930796842f2c
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-04 12:23:03 +0530

    add commit

---
+++ Days.js
@@ -0,0 +1,18 @@
+function checkPalindrome() {
+  const str = "civic";
+  let arr = str.split("").reverse().join("");
+  if (str == arr) {
+    console.log("It is palindrome");
+  } else {
+    console.log("It is not palindrome");
+  }
+}
+checkPalindrome();
+
+//secound largest element in arr
+// function secoundArry() {
+//     const arr = ;
+//     for(let i = 0; i<arr.length; i++){
+//         arr.split('').
+//     }
+// }
