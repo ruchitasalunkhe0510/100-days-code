135cb379532ee3f50d3cd64d05d382d63719df24
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-06 19:40:21 +0530

    find missing element in array

--- Day28.js
+++ Day28.js
@@ -4,16 +4,40 @@ const index = letter.indexOf("d");
 console.log(index);
 
 //Fibonacci Series (0,1,1,2,3,5,8,13....)
-function fibonacciSeries() {
-  const number = parseInt(prompt("Enter the number of terms: "));
-  let n1 = 0,
-    n2 = 1,
-    nextTerm;
-  for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
+// function fibonacciSeries() {
+//   const number = parseInt(prompt("Enter the number of terms: "));
+//   let n1 = 0,
+//     n2 = 1,
+//     nextTerm;
+//   for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+//   }
+// }
+// fibonacciSeries();
+
+// Fibonacci Series (0,1,1,2,3,5,8,13....) where keeping in array
+function listFabonicci(n) {
+  var arr = [0, 1];
+  for (let i = 1; i < n; i++) {
+    arr.push(arr[i] + arr[i - 1]);
+    return arr;
   }
 }
-fibonacciSeries();
+console.log(listFabonicci(5));
+
+//Finding a missing elements in an array and then add with existing elements. (-1 means if elements not found then it will return always -1 as per rule)
+function missingElement() {
+  var a = [1, 2, 3, 5];
+  var missing = [];
+  for (var i = 1; i <= 6; i++) {
+    if (a.indexOf(i) == -1) {
+      missing.push(i);
+    }
+  }
+  console.log(missing);
+  console.log(a.concat(missing).sort());
+}
+missingElement();
