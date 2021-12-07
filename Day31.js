052b9d59cf57fd0c2de05979aebd79a997a4ae39
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-07 17:15:54 +0530

    add commit

---
+++ Day31.js
@@ -0,0 +1,5 @@
+//To check ending of the string with given character/s using inbuild function
+function confirmEnding(str, target) {
+  return str.endsWith(target);
+}
+console.log(confirmEnding("rucha", "a"));
