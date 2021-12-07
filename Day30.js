c6282e7ae71f675ebd814396e250ea30bd73f9be
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-07 12:46:32 +0530

    sum of two largest number of array

--- Day30.js
+++ Day30.js
@@ -8,3 +8,10 @@ function longestWord() {
   console.log(arr[0].length); //11
 }
 longestWord();
+
+//To find the largest pair of the 2 elements using indexing with sorted elements
+function largestPairSumOfTwo(num) {
+  return num[num.length - 1] + num[num.length - 2];
+}
+const result = largestPairSumOfTwo([1, 2, 3, 5, 4, 2, 9, 7]);
+console.log(result); //9+7=16
