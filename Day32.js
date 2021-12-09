e2cd61490c69a9d8a63924c6cce98e169cab63eb
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-09 15:18:45 +0530

    add commit

--- Day32.js
+++ Day32.js
@@ -13,3 +13,25 @@ function firstDuplicate() {
   return -1;
 }
 console.log(firstDuplicate());
+
+//To find the largest elements fro the 2 diensional array
+function largestArray(arr) {
+  var max = [];
+  for (let i = 0; i < arr.length; i++) {
+    var tempMax = arr[i][0];
+    for (var j = 0; j < arr[i].length; j++) {
+      var currElement = arr[i][j];
+      if (currElement >= tempMax) {
+        tempMax = currElement;
+      }
+    }
+    max.push(tempMax);
+  }
+  console.log(max);
+  return max;
+}
+largestArray([
+  [1, 2, 4, 5],
+  [3, 4, 2, 5],
+  [9, 7, 3],
+]);
