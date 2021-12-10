cb130369fe2066ba50065c4e63a2055056621c15
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-10 12:12:07 +0530

    add commit

--- Day33.js
+++ Day33.js
@@ -4,3 +4,14 @@ function repeatStringNumTime(str, num) {
   return str.repeat(num);
 }
 console.log(repeatStringNumTime("rucha", 5)); //rucharucharucharucharucha
+
+// Print string n times in custom way
+function repeateString(str, num) {
+  var final = "";
+  if (num < 0) return "";
+  for (var i = 0; i < num; i++) {
+    final = final + str;
+  }
+  return final;
+}
+console.log(repeateString("priya", 3)); //priyapriyapriya
