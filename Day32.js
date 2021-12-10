54595c5aa26673f64b60248c979c16a0546c11ea
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-10 11:50:57 +0530

    add commit

--- Day32.js
+++ Day32.js
@@ -35,3 +35,23 @@ largestArray([
   [3, 4, 2, 5],
   [9, 7, 3],
 ]);
+
+//To find the largest elements fro the 2 diensional array in another way
+function largestArraydimenstion(arr) {
+  var max = [0, 0, 0, 0];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] >= max[i]) {
+        max[i] = arr[i][j];
+      }
+    }
+  }
+  console.log(max);
+  return max;
+}
+largestArraydimenstion([
+  [1, 2, 3, 4],
+  [5, 6, 7, 9],
+  [45, 76, 2, 1],
+  [89, 90, 87, 9],
+]);
