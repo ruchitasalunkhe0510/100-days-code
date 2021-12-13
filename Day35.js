4ccc8ffdac84e9994f9efe447cc4a3f3b1ff251f
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-12-13 17:13:16 +0530

    find true value filter

--- Day35.js
+++ Day35.js
@@ -8,4 +8,12 @@ function removeFalseValue(arr) {
   }
   return thruely;
 }
-console.log(removeFalseValue(["rucha", 6, 0, "", false, undefined, "ate"])); //[ 'rucha', 6, 'ate' ]
+console.log(removeFalseValue(["rucha", 6, 0, 1, "", false, undefined, "ate"])); //[ 'rucha', 6, 'ate' ]
+
+//To find only truthy values using filter
+function removeValue(arr) {
+  return arr.filter((item) => {
+    return item;
+  });
+}
+console.log(removeValue(["ruchita", 0, 23, undefined, NaN, "hi"])); //[ 'ruchita', 23, 'hi' ]
